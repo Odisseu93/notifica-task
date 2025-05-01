2bed9b3b0324a142aa60af4a125ffa929ec323db
@Odisseu93 - Ulisses Silvério <ulissessuporteti01@gmail.com>
2025-05-01 15:18:34 -0300

    feat: add all files

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+	return (
+		<div className='app'>
+			<h1 className='title font-bebas-neue-regular'>Notifica Task</h1>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa excepturi in itaque impedit eos
+				voluptatibus saepe, earum blanditiis, officiis mollitia ipsa sit, vero ipsum praesentium voluptatem
+				sequi sint molestiae porro.
+			</p>
+		</div>
+	)
+}
+
+export default App
