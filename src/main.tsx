0828d8b033d4117e5ecf330fccab8b3d941ef8d3
@Odisseu93 - Ulisses Silvério <ulissessuporteti01@gmail.com>
2025-06-15 10:23:27 -0300

    feat(main.tsx): add the app within the context of `HashRouter`

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<App />
+		<HashRouter>
+			<App />
+		</HashRouter>
 	</React.StrictMode>
 )
-
-// Use contextBridge
-window.ipcRenderer.on('main-process-message', (_event, message) => {
-	console.log('message', message)
-})
