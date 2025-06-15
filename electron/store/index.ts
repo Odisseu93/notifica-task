801e9655c7bae65dc9b1a70f647b80e86ea6be64
@Odisseu93 - Ulisses Silvério <ulissessuporteti01@gmail.com>
2025-06-15 11:19:49 -0300

    refactor(store): adjust import of type AlarmSoundKeyType

--- electron/store/index.ts
+++ electron/store/index.ts
@@ -1,7 +1,7 @@
 import Store from 'electron-store'
 import { Note } from '../../interfaces/note-interface'
 import { NoteNotification } from '../../interfaces/note-notification-interface'
-import { AlarmSoundKeyType } from '@/libs/notification'
+import { AlarmSoundKeyType } from '@/libs/app-notification'
 
 export default new Store<{
 	notesNotification: Record<string, NoteNotification>
